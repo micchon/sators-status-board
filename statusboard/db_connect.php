c1e752df68b58997c0b6f592f19919ae3a6dbfa8
satorsdotcom <satorsdotcom@gmail.com>
2011-03-03 15:43:33 +0000

    Initial Commit

---
+++ statusboard/db_connect.php
@@ -0,0 +1,11 @@
+<?php
+// **********************************
+// *     Code by Matt Satorius      *
+// *        www.sators.com          *
+// **********************************
+
+// Database Connection User/Pass. Enter your MySQL info here
+mysql_connect("server", "username", "password") or die(mysql_error()); 
+// Database Selection
+mysql_select_db("database") or die(mysql_error()); 
+?>
